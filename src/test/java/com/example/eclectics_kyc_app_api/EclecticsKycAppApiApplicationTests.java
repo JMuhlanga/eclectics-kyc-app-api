de3d3a195dfed2741d6515048bc15d06f0d76438
Jose Kupeka Muhlanga <muhlangakupeka@outlook.com>
2024-12-29 18:20:33 +0300

    Initial Commit

---
+++ src/test/java/com/example/eclectics_kyc_app_api/EclecticsKycAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eclectics_kyc_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclecticsKycAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
